436916c1f3379a0fe1f00b4d891b58e7005babce
MRS\luka.janocik <luka.janocik@mrs-electronic.com>
2024-04-04 15:09:09 +0200

    folder structure

--- crime_spotter/lib/main.dart
+++ crime_spotter/lib/main.dart
@@ -4,78 +4,11 @@ import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
+import 'src/features/LogIn/presentation/login.dart';
+import 'src/shared/4data/const.dart';
+
 void main() {
   runApp(const MyApp());
-}
-
-//Auslagern!!!!!!!!!!!!!!!
-class UIData {
-  //Verwendete Routen
-  static const String homeRoute = "/home";
-  static const String logIn = "/login";
-}
-
-//Login
-class LogIn extends StatefulWidget {
-  const LogIn({super.key});
-
-  @override
-  State<LogIn> createState() => _LogIn();
-}
-
-class _LogIn extends State<LogIn> with SingleTickerProviderStateMixin {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      Navigator.pushNamed(context, UIData.homeRoute);
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
-    );
-  }
 }
 
 class MyApp extends StatelessWidget {
@@ -104,15 +37,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -123,28 +47,6 @@ class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
   late AnimationController controller;
   late Animation<double> animation;
-  bool _isMenuOpened = false;
-
-  void _switchMenu() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _isMenuOpened = !_isMenuOpened;
-    });
-  }
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   controller = AnimationController(
-  //     vsync: this,
-  //     duration: const Duration(seconds: 2),
-  //   );
-  //   animation = Tween<double>(begin: 0.0, end: 1.0).animate(controller);
-  // }
 
   @override
   Widget build(BuildContext context) {
---
+++ crime_spotter/lib/src/shared/4data/const.dart
@@ -0,0 +1,5 @@
+class UIData {
+  //Verwendete Routen
+  static const String homeRoute = "/home";
+  static const String logIn = "/login";
+}
