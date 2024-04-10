234fdc5962650f37fce60aea31ed0ec755853e81
MRS\luka.janocik <luka.janocik@mrs-electronic.com>
2024-04-10 13:18:24 +0200

    einfach mal machen

--- crime_spotter/lib/src/shared/4data/const.dart
+++ crime_spotter/lib/src/shared/4data/const.dart
@@ -3,4 +3,7 @@ class UIData {
   static const String homeRoute = "/home";
   static const String logIn = "/login";
   static const String register = "/register";
+  static const String explore = "/explore";
 }
+
+const String googleMapsApiKey = "AIzaSyCJbAjIZqv32gJ4BeiuomscFObUAUGe-AM";
