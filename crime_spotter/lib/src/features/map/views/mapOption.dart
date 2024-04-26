1363565fd4df376bbd4f7787fb3d32884bdc72f7
MRS\luka.janocik <luka.janocik@mrs-electronic.com>
2024-04-26 03:08:17 +0200

    mapOptions started

--- crime_spotter/lib/src/features/map/views/mapOption.dart
+++ crime_spotter/lib/src/features/map/views/mapOption.dart
@@ -10,8 +10,120 @@ class TMapOption extends StatefulWidget {
 }
 
 class _TMapOptionState extends State<TMapOption> {
+  final double? spaceBetweenOptions = 8;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    'Optionen',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  const Divider(
+                    thickness: 3,
+                  ),
+                  buildMapOptions(),
+                ],
+              ),
+            ),
+            GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: const Icon(Icons.clear),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildMapOptions() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Karternoptionen',
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+          ),
+        ),
+        buildDivider(text: 'Kartenoptionen'),
+        const Text(
+          'Karternoptionen',
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+          ),
+        ),
+        SizedBox(
+          height: spaceBetweenOptions,
+        ),
+        const Text(
+          'Karternoptionen',
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+          ),
+        ),
+        SizedBox(
+          height: spaceBetweenOptions,
+        ),
+        const Text(
+          'Karternoptionen',
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+          ),
+        ),
+        SizedBox(
+          height: spaceBetweenOptions,
+        ),
+        const Text(
+          'Karternoptionen',
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+          ),
+        ),
+        SizedBox(
+          height: spaceBetweenOptions,
+        ),
+      ],
+    );
+  }
+
+  Widget buildDivider({required String text}) {
+    return Row(
+      children: <Widget>[
+        const Expanded(child: Divider()),
+        const SizedBox(
+          width: 5,
+        ),
+        Text(text),
+        const SizedBox(
+          width: 5,
+        ),
+        const Expanded(child: Divider()),
+      ],
+    );
   }
 }
